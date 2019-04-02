b24afa70d362ae2d77f160e0a6cece56819f2ca3
Lifazul Tonmoy <tonmoy.lifazul@gmail.com>
2019-04-02 00:32:06 -0400

    Removed commented code

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -5,14 +5,6 @@ import { Link } from 'react-router-dom';
 export default class Greeting extends React.Component {
   constructor(props) {
     super(props);
-    // this.state = {
-    //   loggedIn: false
-    // }
-    //
-    // if (this.props.currentUser) {
-    //   this.state.loggedIn = true;
-    // }
-    //
     this.props.fetchPostings();
     if (this.props.currentUser) {
       this.props.fetchSubmits();
